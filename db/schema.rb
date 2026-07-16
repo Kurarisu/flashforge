9c33c17e15311780987249857cadd3401003cf37
Fajar Juliansyah <juliansyahfajar19@gmail.com>
2026-07-16 09:51:42 +0700

    Upgrade Rails to 8.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_15_033108) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_16_024029) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "flash_sales", force: :cascade do |t|
-    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "end_at"
     t.decimal "flash_price"
     t.integer "flash_stock"
+    t.bigint "product_id", null: false
     t.datetime "start_at"
-    t.datetime "end_at"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_flash_sales_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
     t.bigint "product_id", null: false
     t.integer "quantity", default: 1, null: false
-    t.decimal "unit_price", precision: 10, scale: 2, null: false
     t.decimal "total_price", precision: 12, scale: 2, null: false
-    t.datetime "created_at", null: false
+    t.decimal "unit_price", precision: 10, scale: 2, null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.bigint "voucher_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
@@ -40,30 +40,30 @@ ActiveRecord::Schema[7.2].define(version: 2026_07_15_033108) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.decimal "price", precision: 10, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "name", null: false
+    t.decimal "price", precision: 10, scale: 2, default: "0.0", null: false
     t.integer "stock", default: 0, null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "vouchers", force: :cascade do |t|
     t.string "code", null: false
-    t.decimal "discount", precision: 5, scale: 4, default: "0.0", null: false
-    t.integer "max_usage", default: 1, null: false
-    t.integer "used_count", default: 0, null: false
-    t.datetime "expired_at"
     t.datetime "created_at", null: false
+    t.decimal "discount", precision: 5, scale: 4, default: "0.0", null: false
+    t.datetime "expired_at"
+    t.integer "max_usage", default: 1, null: false
     t.datetime "updated_at", null: false
+    t.integer "used_count", default: 0, null: false
     t.index ["code"], name: "index_vouchers_on_code", unique: true
   end
 
